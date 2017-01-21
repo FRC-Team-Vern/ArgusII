b693d50edf8e7f59801d57513d77ba4e526cb33e
pizzajoy <letizia.moro@gmail.com>
2017-01-21 16:45:38 -0700

    demonized

--- src/led_test.cpp
+++ src/led_test.cpp
@@ -1,15 +1,27 @@
 #include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
 #include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <syslog.h>
 #include <string.h>
 
-void main( void );
+using namespace std;
+
+#define DAEMON_NAME "vdaemon"
+
+//void main( void );
 void update_pixel( int pixel, unsigned char r, unsigned char g, unsigned char b );
 void write_strip( void );
+void process();
+
+
 
 
 //SPI interface
@@ -90,7 +102,17 @@ void write_strip( void ) {
 
 
 
-void main( void ) {
+
+
+
+void process(){
+
+    syslog (LOG_NOTICE, "Writing to my Syslog");
+}
+
+
+
+void runLEDs( void ) {
 
 
     //SPI setup
@@ -260,5 +282,51 @@ void main( void ) {
 
 
 
+}
+int main(int argc, char *argv[]) {
+
+    //Set our Logging Mask and open the Log
+    setlogmask(LOG_UPTO(LOG_NOTICE));
+    openlog(DAEMON_NAME, LOG_CONS | LOG_NDELAY | LOG_PERROR | LOG_PID, LOG_USER);
+
+    syslog(LOG_INFO, "Entering Daemon");
+
+    pid_t pid, sid;
+
+   //Fork the Parent Process
+    pid = fork();
+
+    if (pid < 0) { exit(EXIT_FAILURE); }
+
+    //We got a good pid, Close the Parent Process
+    if (pid > 0) { exit(EXIT_SUCCESS); }
+
+    //Change File Mask
+    umask(0);
+
+    //Create a new Signature Id for our child
+    sid = setsid();
+    if (sid < 0) { exit(EXIT_FAILURE); }
+
+    //Change Directory
+    //If we cant find the directory we exit with failure.
+    if ((chdir("/")) < 0) { exit(EXIT_FAILURE); }
+
+    //Close Standard File Descriptors
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+
+    //----------------
+    //Main Process
+    //----------------
+    while(true){
+        process();    //Run our Process
+        runLEDs();
+        sleep(60);    //Sleep for 60 seconds
+    }
+
+    //Close the log
+    closelog ();
 }
 
